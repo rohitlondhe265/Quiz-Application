97a97dec7c003e35aebd8c294919717c8753bfd9
Rohit Londhe <rohitlondhe446@gmail.com>
2022-11-12 17:21:47 +0530

    Project is Ready

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        margin: 6,
+        backgroundColor: "black",
+        color: "white",
+        padding:12,
+      }}
+    >
+      Made with ♥ by Rohit Londhe
+    </div>
+  );
+};
+
+export default Footer;
